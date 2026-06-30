ac0ebf199c63d1a3ba699bbbd4199f0b7ab2f54e
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-06-30 00:20:14 -0300

    adiciona configurações do ESLint e plugins para melhorar a qualidade do código

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,17 @@
 import js from "@eslint/js";
-import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+import jsdoc from "eslint-plugin-jsdoc";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
+  js.configs.recommended,
+  jsdoc.configs["flat/recommended"],
+
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
+
+  eslintConfigPrettier,
 ]);
